93c7abd8d6589045f20d81a3cec46a5533c5ba25
Shak <8shak.s@gmail.com>
2019-03-09 11:38:17 -0500

    finshed projects, attempting to present resume on website

--- src/components/home/Projects.js
+++ src/components/home/Projects.js
@@ -7,9 +7,30 @@ export default class Projects extends Component {
       {
         name: "Crypto-Net",
         desc:
-          "A social-network/fourm for crypto fanatics to discuess everything from margin trading bitcoin on bitmex to new technology emerging in the crypto space. IT uses the MERN stack(ReactJS, ReduxJS, NodeJS, ExpressJS, and MongoDB. It uses AWS for storage and passport and the JWT strategy for authentication)",
+          "A social-network/fourm for crypto fanatics to discuess everything from margin trading bitcoin on bitmex to new technology emerging in the crypto space. It uses the MERN stack(ReactJS, ReduxJS, NodeJS, ExpressJS, and MongoDB. It uses AWS for storage and passport and the JWT strategy for authentication). This website is responsive and mobile friendly.",
         url: "https://crypto-net.herokuapp.com/",
         image: require("../../images/crypto-net.gif")
+      },
+      {
+        name: "Bootstrap Template",
+        desc:
+          "A simple template website filled with dummy text. Made with HTML and CSS, using Bootstrap as a UI framework. This website is responsive and mobile friendly.",
+        url: "https://8shaks.github.io/BootstrapLayout.github.io/",
+        image: require("../../images/bootstrapTemp.gif")
+      },
+      {
+        name: "Pong Page",
+        desc:
+          "A website that has some information about pong. The main attraction of this website is the ability to actually play pong when you visit it on desktop. You cannot play the game on mobile however. The webpage was made with HTML and CSS while the game was made with JavaScript.",
+        url: "https://8shaks.github.io/PongGame.github.io/",
+        image: require("../../images/pong.gif")
+      },
+      {
+        name: "SneakerBloc",
+        desc:
+          "A mockup of a online marketplace for sneakers. This web app also uses the MERN stack as well. ",
+        url: "https://sneakerbloc.herokuapp.com/",
+        image: require("../../images/sneakerBloc.gif")
       }
     ];
     return (
@@ -32,7 +53,11 @@ export default class Projects extends Component {
                 </a>
                 <div className="project-desc"> {project.desc}</div>
                 <div className="image">
-                  <img className="project-preview" src={project.image} />
+                  <img
+                    className="project-preview"
+                    src={project.image}
+                    alt="Project preview"
+                  />
                 </div>
               </List.Item>
             </div>
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import "./index.css";
+import { Icon } from "antd";
 import Fade from "react-reveal/Fade";
 import Skills from "./Skills";
 import Projects from "./Projects";
+import { Document } from "react-pdf";
 
 class Home extends Component {
   render() {
@@ -14,17 +16,31 @@ class Home extends Component {
             <div style={{ fontSize: "0.5em" }}>Full Stack Web Developer</div>
           </div>
           {/* Body */}
-          {/* About Me */}
+          {/* About Me
           <div className="about-me">
             <div className="title">About Me</div>
             <div className="text center">
               I am a self taught full stack web developer
             </div>
-          </div>
+          </div> */}
           {/* Skills */}
           <Skills />
           {/* Projects */}
           <Projects />
+          {/* Resume/ Contact me */}
+          <div className="contact-me">
+            <div className="title">Contact Me</div>
+            <div className="text center">
+              You can email me about any questions you have at
+              shakiran.sathiyanathan@gmail.com
+              <br />
+              My resume is available for download below
+              <span>
+                <Icon type="arrow-down" />{" "}
+              </span>
+              <Document file={require("../../images/resume.pdf")} />
+            </div>
+          </div>
         </Fade>
       </div>
     );
