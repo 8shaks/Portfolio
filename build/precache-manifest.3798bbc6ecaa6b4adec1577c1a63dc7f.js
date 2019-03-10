36b114443cb64ba529cccf4e58f859de9b8e711f
Shak <8shak.s@gmail.com>
2019-03-10 12:58:41 -0400

    Created build folder

---
+++ build/precache-manifest.3798bbc6ecaa6b4adec1577c1a63dc7f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "355bb7c31857ebc3e8af6704e407f93a",
+    "url": "/static/media/sneakerBloc.355bb7c3.gif"
+  },
+  {
+    "revision": "8a96e574a846202aa5b557c0d0c1f6c4",
+    "url": "/static/media/pong.8a96e574.gif"
+  },
+  {
+    "revision": "2f4b2c33e50d8c819deb076986a37fbf",
+    "url": "/static/media/crypto-net.2f4b2c33.gif"
+  },
+  {
+    "revision": "8be0b91c78d3263c0560e080a2709cb9",
+    "url": "/static/media/bootstrapTemp.8be0b91c.gif"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4cbd095355644fe2c6b7",
+    "url": "/static/js/main.b7796812.chunk.js"
+  },
+  {
+    "revision": "0247a84ee004ba17d868",
+    "url": "/static/js/2.ab5eb25e.chunk.js"
+  },
+  {
+    "revision": "4cbd095355644fe2c6b7",
+    "url": "/static/css/main.610009bf.chunk.css"
+  },
+  {
+    "revision": "7ee44110ee325b37cd78b690dd54d9d6",
+    "url": "/index.html"
+  }
+];
