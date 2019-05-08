3001c993f6a46dd22610489b39684cb930ba730a
Shakiran <8shak.s@gmail.com>
2019-05-08 15:47:18 -0400

    Fixed keys bug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
-const keys = require("./config/keys_dev");
+const keys = require("./config/keys");
 const validateInput = require("./validation");
 const path = require("path");
 
